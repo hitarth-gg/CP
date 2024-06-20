ca696cf92dc0a924fbcfe428ef07f1a56bf4d173
hitarth <goforhitarth@gmail.com>
2024-06-20 22:44:33 +0530

    ++

---
+++ Headers/debug1.h
@@ -0,0 +1,43 @@
+#ifndef DEBUG_TEMPLATE_H
+#define DEBUG_TEMPLATE_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define trace1(x)                cerr << #x << ": " << x << endl
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x) { cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}'; }
+template <typename T>
+void __print(const T &x) { int f = 0; cerr << '{'; for (const auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}"; }
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v) { __print(t); if (sizeof...(v)) cerr << ", "; _print(v...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#endif // DEBUG_TEMPLATE_H
